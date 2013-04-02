40c267d8df233ffee4fe29557fa106aa1211b741
David Orr <dorr91@gmail.com>
2013-04-02 05:48:45 -0400

    Update so it won't run if you don't pass a number of threads.

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ import java.io.IOException;
 
 public class Main {
 	
-	static int maxThreads = 20;
+	static int maxThreads = 4;
 	
 	public static void main(String [] args) throws IOException {
-		if(args.length > 0) {
+		if(args.length == 0) System.out.println("Please specify the number of threads to use.");
+		else if(args.length > 0) {
 			maxThreads = Integer.parseInt(args[0]);
 		}
 		System.out.println("Starting " + maxThreads + " threads.");
